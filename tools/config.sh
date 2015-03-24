3bc9127ab096392604b1c5a3cc5a26cf5d966725
Chimwemwe <chimwemwe.kachaje@baobabhealth.org>
2015-03-24 17:00:54 +0200

    Made some modifications to the initialisation script

--- tools/config.sh
+++ tools/config.sh
@@ -6,11 +6,9 @@ adb start-server
 
 adb devices
 
-adb remount /system/etc/permissions/handheld_core_hardware.xml rw
+sleep 1
 
-adb push handheld_core_hardware.xml /system/etc/permissions/handheld_core_hardware.xml
-
-adb shell pm disable com.android.launcher
+adb shell pm disable com.google.android.apps.maps
 
 adb shell pm disable com.android.systemui
 
@@ -19,6 +17,8 @@ adb shell pm disable com.android.browser
 adb shell pm disable com.android.calculator2
 
 adb shell pm disable com.android.calendar
+
+adb shell pm disable com.android.camera
 
 adb shell pm disable com.android.deskclock
 
@@ -36,19 +36,34 @@ adb shell pm disable com.google.android.gm
 
 adb shell pm disable com.google.android.gms
 
-adb shell pm disable com.google.android.apps.maps
-
 adb shell pm disable com.android.music
 
 adb shell pm disable com.mxtech.videoplayer.ad
 
 adb shell pm disable com.android.notepad
 
-adb shell pm disable com.android.vending
-
 adb shell pm disable com.android.fmradio
 
 adb uninstall org.baobabhealth.myusb.myusb
 
 adb install app-release.apk
+
+adb shell pm disable com.android.vending
+
+# adb shell pm clear com.android.launcher
+
+# adb pull /system/app/MarketUpdater.apk
+# adb pull /system/app/GooglePlay.apk .
+# adb remount /system/app/GooglePlay.apk rw
+# adb shell rm /system/app/GooglePlay.apk
+
+sleep 1
+
+adb shell am start -n org.baobabhealth.myusb.myusb/.shortcut
+
+adb remount /system/etc/permissions/handheld_core_hardware.xml rw
+
+adb push handheld_core_hardware.xml /system/etc/permissions/handheld_core_hardware.xml
+
+adb shell reboot
 
