acfc4aff9c602a4fefa3bb011a470c8f186a61e2
Kenneth <kennethkapundi1@gmail.com>
2015-04-13 16:32:09 +0200

    added functionality for changing password

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -55,4 +55,54 @@ class SessionsController < ApplicationController
     return user_names
   end
 
+  def change_password
+
+    @section = params[:section]
+
+    if request.post?
+
+      @username = session[:user]
+
+      @current_password = params[:current_password]
+
+      @new_password = params[:new_password]
+
+      @repeat_password = params[:repeat_password]
+
+      @username = session[:user]
+
+      @section = params[:section]
+
+      status = ""
+
+      params = {:username => @username, :new_password => @new_password, :repeat_password => @repeat_password}
+
+      if !@section.blank?
+        login_link = "#{CONFIG["user_management_protocol"]}://#{@username}:#{@current_password}@#{CONFIG["user_management_server"]}:#{CONFIG["user_management_port"]}#{CONFIG["user_management_user_update_path"]}"
+      else
+        login_link = "#{CONFIG["order_transport_protocol"]}://#{@username}:#{@current_password}@#{CONFIG["order_server"]}:#{CONFIG["order_port"]}#{CONFIG["order_server_user_update_path"]}"
+      end
+
+      status = JSON.parse(RestClient.post(login_link, params)) rescue "Error: Failed to connect to server!"
+
+      if (!status['ERROR'].blank? rescue false)
+        status = status['ERROR']
+      end
+
+      if status.match(/error/i)
+        flash[:error] = status.gsub("Error: ", "")
+      else
+        flash[:notice] = "Update successful"
+
+        if @section.blank?
+          redirect_to "/login"
+        else
+          redirec_to "/remote_login"
+        end
+
+      end
+    end
+
+  end
+
 end
